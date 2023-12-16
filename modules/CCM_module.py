a9ad48980c19cb33313435b180b8215911fdda9c
AlexYouXin <48478426+AlexYouXin@users.noreply.github.com>
2023-12-16 12:41:04 +0800

    Update CCM_module.py

--- modules/CCM_module.py
+++ modules/CCM_module.py
@@ -11,6 +11,110 @@ from torch.nn import CrossEntropyLoss, Dropout, Softmax, Linear, Conv2d, Conv3d,
 
 
 
+def swish(x):
+    return x * torch.sigmoid(x)
+
+ACT2FN = {"gelu": torch.nn.functional.gelu, "relu": torch.nn.functional.relu, "swish": swish}
+
+
+class Attention(nn.Module):
+    def __init__(self, config):
+        super(Attention, self).__init__()
+        self.num_attention_heads = config.transformer.num_heads
+        self.attention_head_size = int(config.dim / self.num_attention_heads)  # 768 / 6 = 128
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = Linear(config.dim, self.all_head_size)
+        self.key = Linear(config.dim, self.all_head_size)
+        self.value = Linear(config.dim, self.all_head_size)
+
+        self.out = Linear(config.dim, config.dim)
+        self.attn_dropout = Dropout(config.transformer["attention_dropout_rate"])
+        self.proj_dropout = Dropout(config.transformer["attention_dropout_rate"])
+
+        self.softmax = Softmax(dim=-1)
+        self.position_embeddings = nn.Parameter(
+            torch.randn(1, self.num_attention_heads, config.n_classes, config.n_classes))
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, hidden_states):
+        mixed_query_layer = self.query(hidden_states)
+        mixed_key_layer = self.key(hidden_states)
+        mixed_value_layer = self.value(hidden_states)
+
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+        key_layer = self.transpose_for_scores(mixed_key_layer)
+        value_layer = self.transpose_for_scores(mixed_value_layer)
+
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+
+        attention_scores = attention_scores + self.position_embeddings  # RPE
+
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        attention_probs = self.softmax(attention_scores)
+        # weights = attention_probs if self.vis else None
+        attention_probs = self.attn_dropout(attention_probs)
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+        attention_output = self.out(context_layer)
+        attention_output = self.proj_dropout(attention_output)
+        return attention_output
+
+
+class Mlp(nn.Module):
+    def __init__(self, config):
+        super(Mlp, self).__init__()
+        self.fc1 = Linear(config.dim, config.hidden_size)
+        self.fc2 = Linear(config.hidden_size, config.dim)
+        self.act_fn = ACT2FN["gelu"]
+        self.dropout = Dropout(config.transformer["dropout_rate"])
+
+        self._init_weights()
+
+    def _init_weights(self):
+        nn.init.xavier_uniform_(self.fc1.weight)
+        nn.init.xavier_uniform_(self.fc2.weight)
+        nn.init.normal_(self.fc1.bias, std=1e-6)
+        nn.init.normal_(self.fc2.bias, std=1e-6)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act_fn(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = self.dropout(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super(Block, self).__init__()
+
+        self.attention_norm = LayerNorm(config.dim, eps=1e-6)
+        self.ffn_norm = LayerNorm(config.dim, eps=1e-6)
+        self.ffn = Mlp(config)
+        self.attn = Attention(config)
+
+    def forward(self, x):
+        h = x
+        x = self.attention_norm(x)
+        x = self.attn(x)
+        x = x + h
+
+        h = x
+        x = self.ffn_norm(x)
+        x = self.ffn(x)
+        x = x + h
+        return x
+
+
 class self_attention(nn.Module):
     def __init__(self, config):
         super(self_attention, self).__init__()
